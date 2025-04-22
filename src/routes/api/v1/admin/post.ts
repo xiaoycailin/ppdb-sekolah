f406989d70102bcb731c03a8fcabd6cbd9be8fc1
xiaoycailin <denisprasetio060@gmail.com>
2025-04-22 16:56:34 +0700

    sore

--- src/routes/api/v1/admin/post.ts
+++ src/routes/api/v1/admin/post.ts
@@ -1,63 +1,28 @@
 import { Router } from "express";
-import { authMiddleware } from "../../../../service/authMiddleware";
-import { validationMidlerware } from "../../../../service/validationMidleware";
 import { db } from "../../../../database";
+import { authMiddleware } from "../../../../service/authMiddleware";
+import { User } from "@prisma/client";
+import express from 'express';
 
 const route = Router()
-
-route.post('/', authMiddleware as any, validationMidlerware as any, async (req, res, next) => {
+route.use(express.json())
+route.post('/', authMiddleware as any, async (req, res, next) => {
     try {
-        const body = req.body
-       const datacreate = await db.post.create({
+        const userData = (req as any).user as User
+        const post = await db.post.create({
             data: {
-                content: 'artikel nya apa',
-                authorId: (req as any).user.id,
-                slug: 'slug nya apa',
-                thumbnail: 'thumbnialnya apa',
-                title: 'title nya apa'
+                authorId: userData.id,
+                ...req.body
             }
         })
-        res.json({
+
+        res.send({
             success: true,
+            data: post
         })
     } catch (error) {
         next(error)
     }
 })
 
-
-// post data /  create data
-// db.post.create({
-//     data: {
-//         content: 'artikel nya apa',
-//         authorId: 'author id nya siapa',
-//         slug: 'slug nya apa',
-//         thumbnail: 'thumbnialnya apa',
-//         title: 'title nya apa'        
-//     }
-// })
-
-// ini untk update data
-// db.post.update({
-//     where: {
-//         id: 'post id',
-//     },
-//     data: {
-//         title: 'title yang akan di update'
-//     }
-// })
-
-// ini untuk delete data
-// db.post.delete({
-//     where: {
-//         id: 'postid'
-//     }
-// })
-
-
-// ini untuk get data
-// db.post.findMany()
-
-// semuanya harus pake await
-
-export default route;
+export default route
